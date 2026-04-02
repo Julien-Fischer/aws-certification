467e1b578ae22dee35d49ef24f210a1b78083e31
Julien Fischer <julien.fischer@agiledeveloper.net>
2026-04-02 22:59:58 +0200

    ♻️ More renaming toward flash card abstraction

--- aws-services-app/src/app/ui/app.routes.ts
+++ aws-services-app/src/app/ui/app.routes.ts
@@ -1,9 +1,9 @@
 import { Routes } from '@angular/router';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
-import { RevisionCardComponent } from './components/revision-card/revision-card.component';
+import { FlashCardComponent } from './components/revision-card/flash-card.component';
 
 export const routes: Routes = [
   { path: '', component: DashboardComponent },
-  { path: 'service/:id', component: RevisionCardComponent },
+  { path: 'service/:id', component: FlashCardComponent },
   { path: '**', redirectTo: '' }
 ];
