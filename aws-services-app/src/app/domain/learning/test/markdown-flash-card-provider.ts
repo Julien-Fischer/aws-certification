be149de31b8817645ad99c445275a8d4f03da581
Julien Fischer <julien.fischer@agiledeveloper.net>
2026-04-02 22:48:16 +0200

    ♻️ Rename AwsServicesProvider to FlashCardProvider

--- aws-services-app/src/app/domain/learning/test/markdown-flash-card-provider.ts
+++ aws-services-app/src/app/domain/learning/test/markdown-flash-card-provider.ts
@@ -2,12 +2,12 @@ import { TestBed } from '@angular/core/testing';
 import {Observable, of} from 'rxjs';
 import { describe, it, expect, beforeEach, vi, type Mocked } from 'vitest';
 import { FlashCardService } from '../services/flash-card.service';
-import { awsServicesProviderInjectionToken, AwsServicesProvider } from '../aws-service-provider';
+import { flashCardProviderInjectionToken, FlashCardProvider } from '../aws-service-provider';
 import { FlashCardMetadata } from '../models/aws-service.model';
 import { FlashCard } from '../models/flash-card';
 import {FlashCardId} from "../../shared/FlashCardId";
 
-class MockServiceProvider implements AwsServicesProvider {
+class MockServiceProvider implements FlashCardProvider {
 
   private services: FlashCardMetadata[] = [];
   private revisionCards: Map<FlashCardId, FlashCard> = new Map();
@@ -41,7 +41,7 @@ describe('FlashCardService', () => {
     TestBed.configureTestingModule({
       providers: [
         FlashCardService,
-        { provide: awsServicesProviderInjectionToken, useValue: mockProvider }
+        { provide: flashCardProviderInjectionToken, useValue: mockProvider }
       ]
     });
     service = TestBed.inject(FlashCardService);
