0de11d31921bce05086f74a44f8fd87b3457f4c1
Julien Fischer <julien.fischer@agiledeveloper.net>
2026-04-04 16:21:41 +0200

    ✅ Rename test

--- aws-services-app/src/app/ui/components/flash-card/flash-card.component.spec.tsx
+++ aws-services-app/src/app/ui/components/flash-card/flash-card.component.spec.tsx
@@ -2,7 +2,7 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { describe, it, expect, beforeEach } from 'vitest';
 import {FlashCardComponent} from "./flash-card.component";
 
-describe('ServiceDetailComponent', () => {
+describe('FlashCardComponent', () => {
     let component: FlashCardComponent;
     let fixture: ComponentFixture<FlashCardComponent>;
 
