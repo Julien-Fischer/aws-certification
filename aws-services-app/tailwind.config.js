c91e707be767f0dc74675a3c37cdf995a70f41ae
Julien Fischer <julien.fischer@agiledeveloper.net>
2026-05-19 15:38:30 +0200

    🔧 Setup tailwind breakpoints

--- aws-services-app/tailwind.config.js
+++ aws-services-app/tailwind.config.js
@@ -7,7 +7,14 @@ module.exports = {
         primary: '#529747',
         secondary: '#2687c6',
       },
-    },
+      screens: {
+        xs: '480px',
+        // sm: '576px',
+        // md: '768px',
+        // lg: '992px',
+        // xl: '1200px',
+      }
+    }
   },
   plugins: [],
 }
