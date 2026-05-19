e1f6d56179839aac22291517528f0fc23560d6bb
Julien Fischer <julien.fischer@agiledeveloper.net>
2026-05-19 15:16:15 +0200

    🔧 Clean up tailwind config

--- aws-services-app/tailwind.config.js
+++ aws-services-app/tailwind.config.js
@@ -1,28 +1,13 @@
-const colors = require("tailwindcss/colors");
-
 module.exports = {
   darkMode: 'class',
-  content: [ "./src/**/*.{html,ts}",],
+  content: ["./src/**/*.{html,ts}"],
   theme: {
     extend: {
       colors: {
-        slate: colors.slate,
-        sky: colors.sky,
-        purple: colors.purple,
-      }
+        primary: '#529747',
+        secondary: '#2687c6',
+      },
     },
-    colors: {
-      // primary: '#529747',
-      // secondary: '#2687c6',
-
-      black: colors.black,
-      white: colors.white,
-      gray: colors.gray,
-      red: colors.red,
-      emerald: colors.emerald,
-      indigo: colors.indigo,
-      yellow: colors.yellow,
-    }
   },
   plugins: [],
 }
