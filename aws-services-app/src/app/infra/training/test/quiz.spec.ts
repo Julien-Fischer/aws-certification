0890c949eb8609c2cebd875351311184f3e49f8d
Julien Fischer <julien.fischer@agiledeveloper.net>
2026-06-08 22:36:20 +0200

    ♻️ (training) Lock behavior

--- aws-services-app/src/app/infra/training/test/quiz.spec.ts
+++ aws-services-app/src/app/infra/training/test/quiz.spec.ts
@@ -12,4 +12,33 @@ describe('QuizOutcome', () => {
     expect(outcome.accuracy).toEqual(Percentage.ONE_HUNDRED);
   });
 
+  it.for([0, 49])
+  ('has failed when < 50%', (accuracy: number) => {
+    const outcome = anOutcome().withAccuracy(accuracy);
+
+    expect(outcome.hasFailed()).toBe(true);
+  })
+
+  it.for([50, 99])
+  ('has succeeded when >= 50%', (accuracy: number) => {
+    const outcome = anOutcome().withAccuracy(accuracy);
+
+    expect(outcome.hasSucceeded()).toBe(true);
+  })
+
+  it('has mastered when == 100%', () => {
+    const outcome = anOutcome().withAccuracy(100);
+
+    expect(outcome.hasMastered()).toBe(true);
+  })
+
 })
+
+function anOutcome() {
+  return {
+    withAccuracy(accuracy: number) {
+      return QuizOutcome.from(new Percentage(100), new Percentage(accuracy));
+    }
+  }
+}
+
